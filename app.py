d79f889fe3717dd16e085b7a760982ae3a22afb2
Ricardo1921 <49916582+Ricardo1921@users.noreply.github.com>
2019-06-06 18:21:26 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 def gravar(v1, v2):
     import sqlite3
-    ficheiro = sqlite3.connect('db/Utilizador.db')
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
     db = ficheiro.cursor()
-    db.execute("CREATE TABLE IF NOT EXISTS usr (usr text, pwd text)")
-    db.execute("INSERT INTO usr VALUES (?, ?)", (v1, v2))
+    db.execute("CREATE TABLE IF NOT EXISTS usr (usr txt, pwd txt)")
+    db.execute("INSERT INTO usr VALUES (?,?)", (v1, v2))
     ficheiro.commit()
     ficheiro.close()
     return
@@ -16,40 +16,87 @@ def gravar(v1, v2):
 
 def alterar(v1, v2):
     import sqlite3
-    ficheiro = sqlite3.connect('db/Utilizador.db')
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
     db = ficheiro.cursor()
-    db.execute("UPDATE usr SET pwd = ? WHERE usr = ?", (v2, v1))
+    db.execute("CREATE TABLE IF NOT EXISTS usr (usr txt, pwd txt)")
+    db.execute("UPDATE usr SET pwd = ? WHERE usr = ?", (v1, v2))
     ficheiro.commit()
     ficheiro.close()
     return
 
 
-def existe(v1):
+def existe(v1,):
     import sqlite3
-    ficheiro = sqlite3.connect('db/Utilizador.db')
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
     db = ficheiro.cursor()
-    db.execute("SELECT * FROM usr WHERE usr = ?", (v1,))
+    db.execute("SELECT * FROM usr WHERE usr = ? and pwd= ?", (v1,))
+    valor = db.fetchone()
+    ficheiro.commit()
+    ficheiro.close()
+    return valor
+
+
+def log(v1, v2):
+    import sqlite3
+    ficheiro = sqlite3.connect('db/Utilizadores.db')
+    db = ficheiro.cursor()
+    db.execute("SELECT * FROM usr WHERE usr = ? and pwd = ?", (v1, v2,))
     valor = db.fetchone()
     ficheiro.close()
     return valor
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/newpass', methods=['GET', 'POST'])
+def newpass():
+    erro = None
+    if request.method == 'POST':
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
+        v3 = request.form['cpwd']
+        if not existe(v1):
+            erro = 'O utilizador não existe.'
+        elif v2 != v3:
+            erro = 'A palavra passe não coincide.'
+        else:
+            alterar(v1, v2)
+    return render_template('newpass.html', erro=erro)
+
+
+@app.route('/registo', methods=['GET', 'POST'])
 def registo():
     erro = None
-    if request.method == "POST":
+    if request.method == 'POST':
         v1 = request.form['usr']
         v2 = request.form['pwd']
         v3 = request.form['cpwd']
         if existe(v1):
-            erro = 'O utilizador já existe '
+            erro = 'O utilizador já existe.'
         elif v2 != v3:
             erro = 'A palavra passe não coincide.'
         else:
             gravar(v1, v2)
-            erro = 'Utilizador registado.'
+            erro = 'Utilizador  registado com sucesso'
     return render_template('registo.html', erro=erro)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    erro = None
+    if request.method == 'POST':
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
+        if not existe(v1):
+            erro = 'O utilizador já existe.'
+        elif not log(v1, v2):
+            erro = 'A palavra passe não coincide.'
+        else:
+            erro = 'Bem-vindo'
+    return render_template('login.html', erro=erro)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
